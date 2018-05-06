d88c32f56d4fe36161aabe3522411d2dd8b685bd
Noemi Romero <nlll02@yahoo.com>
2018-05-06 16:03:38 -0500

    set up views/rendered a template

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -2,9 +2,11 @@ class ApplicationController < Sinatra::Base
 
 	require 'bundler'
 	Bundler.require()
+	set :views, File.expand_path('../views', File.dirname(__FILE__))
+
 
 	get '/' do
-		"hey cool the server runs"
+	erb :hello
 	end
 
 end
