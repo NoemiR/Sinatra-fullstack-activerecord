cd2ec4c49320aea423b4ee2246347d26964e2829
Noemi Romero <nlll02@yahoo.com>
2018-05-06 18:30:28 -0500

    Enabled sessions and used it to create messaging

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -3,6 +3,8 @@ class ApplicationController < Sinatra::Base
 	require 'bundler'
 	Bundler.require()
 
+	enable :sessions
+
 	ActiveRecord::Base.establish_connection(
  		:adapter => 'postgresql', 
  		:database => 'item'
--- controllers/ItemController.rb
+++ controllers/ItemController.rb
@@ -32,6 +32,7 @@ class ItemController < ApplicationController
 	@item.title = params[:title]
 	@item.user_id = 1 # for now
 	@item.save
+	session[:message] = "You added item \##{@item.id}."
 
 	# hey there's a .to_json method. cool.
 	@item.to_json
@@ -44,6 +45,8 @@ class ItemController < ApplicationController
 		# and trying stuff out
 		@item = Item.find params[:id]
 		@item.destroy
+		session[:message] = "You deleted item \##{@item.id}"
+		pp session
 		redirect '/items'
 	end
 
@@ -52,6 +55,24 @@ class ItemController < ApplicationController
 		@item = Item.find params[:id]
 		@page = "Edit Item #{@item.id}" #why am i using interpolation here?  try with concatenation and see what happens.
 		erb :edit_item
+	end
+
+	# update route
+	patch '/:id' do
+		# like i said -- lots of ways to do this.  
+		# http://api.rubyonrails.org/classes/ActiveRecord/FinderMethods.html
+		# http://api.rubyonrails.org/classes/ActiveRecord/QueryMethods.html#method-i-where
+		@items = Item.where(id: params[:id]) 
+
+		# note: .where method gives us an array (Why?). So we must index. 
+		# Might there have been a more appropriate query method to use 
+		# instead of .where ?
+		@item = @items[0]
+
+		@item.title = params[:title]
+		@item.save
+		session[:message] = "You updated item \##{@item.id}"
+		redirect '/items'
 	end
 
 
