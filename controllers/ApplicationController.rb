c01345ac95347cc6c01684101bdc8fe42b8bdebd
Noemi Romero <nlll02@yahoo.com>
2018-05-06 18:09:00 -0500

    Set up CSS public folder and moved CSS there

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -7,7 +7,7 @@ class ApplicationController < Sinatra::Base
  		:adapter => 'postgresql', 
  		:database => 'item'
 	)
-
+	set :public_dir, File.expand_path('../public', File.dirname(__FILE__))
 	set :views, File.expand_path('../views', 'hello.erb')
 
 	use Rack::MethodOverride  # we "use" middleware in Rack-based libraries/frameworks
