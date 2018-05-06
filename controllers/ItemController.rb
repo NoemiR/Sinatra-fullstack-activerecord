dcf6cb606c2961f54ee429a583d38b98d435dc6b
Noemi Romero <nlll02@yahoo.com>
2018-05-06 16:29:11 -0500

    Nested form partial

--- controllers/ItemController.rb
+++ controllers/ItemController.rb
@@ -7,7 +7,16 @@ class ItemController < ApplicationController
 
 	# add route 
 	get '/add' do
-		erb :add_item # this view will be created in the next step
+		
+	# this view will be created in the next step
+	@page = "Add Item"
+	@action = "/items"
+	@method = "POST"
+	@placeholder = "Enter your item!"
+	@value=""
+	@buttontext = "Add Item"
+	erb :add_item 
 	end
+	
 
 end
