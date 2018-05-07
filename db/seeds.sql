51088585538e4863a224483e45788bfc7d489ad4
Noemi Romero <nlll02@yahoo.com>
2018-05-06 19:20:47 -0500

    Added bcrypt

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,7 @@
-INSERT INTO users (username, password) VALUES ('Noemi', '11223');
+INSERT INTO users (username, password_digest) VALUES ('Noemi', '11223');
+INSERT INTO users (username, password_digest) VALUES ('Ale', '44526');
+INSERT INTO users (username, password_digest) VALUES ('Lizzy', '17565');
+INSERT INTO users (username, password_digest) VALUES ('Maria', '55656');
+INSERT INTO users (username, password_digest) VALUES ('Kari', '121212');
+INSERT INTO users (username, password_digest) VALUES ('Rey', '09787');
 
